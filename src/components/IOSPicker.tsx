5207d04b65f3ab67045d2eb5f727fdcb3068a5ce
shadysaid <shadysaid@gmail.com>
2026-01-12 10:52:26 +0800

    chore: configure deployment for GitHub Pages

--- src/components/IOSPicker.tsx
+++ src/components/IOSPicker.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { motion, useMotionValue, useTransform, animate } from 'framer-motion';
 
 interface IOSPickerProps {
